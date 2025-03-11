8aeee1d597c42e7b90556c76f86ae2eb2c7da2ea
Juan Carlos López Morales <97jclm@gmail.com>
2025-03-11 21:48:59 +0100

    First tries

---
+++ src/components/Header/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar(){
+
+
+    return (<nav>
+        <ul>
+            <li>IG</li>
+            <li>Tiktok</li>
+        </ul>
+        
+        
+        </nav>
+    );
+}
